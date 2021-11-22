d1f689511097558295b303c89cd63a12dcdbd8df
Emma Leonardi <emma.leonardi2@studio.unibo.it>
2021-11-22 12:23:01 +0100

    Removed unused imports

--- src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
@@ -1,13 +1,11 @@
 package it.unibo.oop.lab.reactivegui02;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.lang.reflect.InvocationTargetException;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
